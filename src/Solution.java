a179be3e7412c72d1607808895dce2fb5a0a87a5
oklsh <oklshop555@naver.com>
2023-02-18 22:57:18 +0900

    조별 경기 제한시간 초과

--- src/Solution.java
+++ src/Solution.java
@@ -1,66 +1,72 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
-    private final static int CMD_INIT = 1;
-    private final static int CMD_ARRIVE = 2;
-    private final static int CMD_LEAVE = 3;
+    private static BufferedReader br;
+    private static final UserSolution userSolution = new UserSolution();
 
-    private final static UserSolution usersolution = new UserSolution();
+    private final static int CMD_INIT = 100;
+    private final static int CMD_UPDATE_SCORE = 200;
+    private final static int CMD_UNION_TEAM = 300;
+    private final static int CMD_GET_SCORE = 400;
 
-    private static boolean run(BufferedReader br) throws Exception {
-        int q = Integer.parseInt(br.readLine());
-
-        int n, mid;
-        int cmd, ans, ret = 0;
+    private static boolean run() throws IOException
+    {
+        int queryCnt = Integer.parseInt(br.readLine());
         boolean okay = false;
+        int res, ans;
 
-        for (int i = 0; i < q; ++i) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            cmd = Integer.parseInt(st.nextToken());
-            switch (cmd) {
+        for (int i = 0; i < queryCnt; i++)
+        {
+            StringTokenizer stdin = new StringTokenizer(br.readLine());
+            switch (Integer.parseInt(stdin.nextToken()))
+            {
                 case CMD_INIT:
-                    n = Integer.parseInt(st.nextToken());
-                    usersolution.init(n);
+                    int N = Integer.parseInt(stdin.nextToken());
+                    userSolution.init(N);
                     okay = true;
                     break;
-                case CMD_ARRIVE:
-                    mid = Integer.parseInt(st.nextToken());
-                    ans = Integer.parseInt(st.nextToken());
-                    ret = usersolution.arrive(mid);
-                    if (ret != ans)
-                        okay = false;
+                case CMD_UPDATE_SCORE:
+                    int mWinnerID = Integer.parseInt(stdin.nextToken());
+                    int mLoserID = Integer.parseInt(stdin.nextToken());
+                    int mScore = Integer.parseInt(stdin.nextToken());
+                    userSolution.updateScore(mWinnerID, mLoserID, mScore);
                     break;
-                case CMD_LEAVE:
-                    mid = Integer.parseInt(st.nextToken());
-                    ans = Integer.parseInt(st.nextToken());
-                    ret = usersolution.leave(mid);
-                    if (ret != ans)
-                        okay = false;
+                case CMD_UNION_TEAM:
+                    int mPlayerA = Integer.parseInt(stdin.nextToken());
+                    int mPlayerB = Integer.parseInt(stdin.nextToken());
+                    userSolution.unionTeam(mPlayerA, mPlayerB);
                     break;
-                default:
-                    okay = false;
+                case CMD_GET_SCORE:
+                    int mID = Integer.parseInt(stdin.nextToken());
+                    res = userSolution.getScore(mID);
+                    ans = Integer.parseInt(stdin.nextToken());
+                    if (ans != res)
+                    {
+                        okay = false;
+                    }
                     break;
             }
         }
+
         return okay;
     }
 
-    public static void main(String[] args) throws Exception {
-        int TC, MARK;
+    public static void main(String[] args) throws IOException {
+         System.setIn(new java.io.FileInputStream("src/sample_input.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stinit = new StringTokenizer(br.readLine(), " ");
 
-        System.setIn(new java.io.FileInputStream("src/sample_input.txt"));
+        int T, MARK;
+        T = Integer.parseInt(stinit.nextToken());
+        MARK = Integer.parseInt(stinit.nextToken());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        TC = Integer.parseInt(st.nextToken());
-        MARK = Integer.parseInt(st.nextToken());
-
-        for (int testcase = 1; testcase <= TC; ++testcase) {
-            int score = run(br) ? MARK : 0;
-            System.out.println("#" + testcase + " " + score);
+        for (int tc = 1; tc <= T; tc++)
+        {
+            int score = run() ? MARK : 0;
+            System.out.printf("#%d %d\n", tc, score);
         }
 
         br.close();
--- src/UserSolution.java
+++ src/UserSolution.java
@@ -1,99 +1,82 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class UserSolution {
-    int size, count;
-    TreeSet<Node> pq;
-    Map<Integer, Integer> find_locker; // mid -> locker
-    Map<Integer, Node> range;
+    Player[] players;
+    Team[] team;
 
     public void init(int N) {
-        pq = new TreeSet<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                if (o1.total == o2.total) {
-                    return o1.s - o2.s;
-                } else return o2.total - o1.total;
-            }
-        });
-        find_locker = new HashMap<>();
-        range = new HashMap<>();
-        size = N;
-        count = size;
-        pq.add(new Node(1, size));
-    }
-
-    public int arrive(int mId) { //입실
-        count--;
-        int idx;
-        Node n = pq.pollFirst();
-        if (n.s == 1) {
-            idx = 1;
-            alloc(new Node(idx + 1, n.e));
-        } else if (n.e == size) {
-            idx = size;
-            alloc(new Node(n.s, size - 1));
-        } else if (n.total == 1) { //한 개 짜리 -> idx 넣고 pq에 추가 x
-            idx = n.s;
-        } else if (n.total == 2) { //두 개 짜리 -> pq 한 칸 짜리 추가됨
-            idx = n.s;
-            alloc(new Node(idx + 1, idx + 1));
-        } else {
-            idx = (n.s + n.e) / 2;
-            Node n1 = new Node(n.s, idx - 1);
-            Node n2 = new Node(idx + 1, n.e);
-            alloc(n1);
-            alloc(n2);
-        }
-        range.put(idx, null);
-        find_locker.put(mId, idx);
-        return idx;
-    }
-
-    public void alloc(Node node) {
-        int s = node.s;
-        int e = node.e;
-        pq.add(node);
-        range.put(s, node);
-        range.put(e, node);
-    }
-
-    public int leave(int mId) {
-        count++;
-        int idx = find_locker.get(mId); //mid의 락커번호, range.get(idx) -> 락커가 속한 구간 노드가 나온다
-        find_locker.put(mId, -1);
-        Node left = range.get(idx - 1);
-        Node right = range.get(idx + 1);
-        if (left == null && right == null) { //양쪽 다 막힘
-            Node n = new Node(idx, idx);
-            alloc(n);
-        } else if (left == null) { //좌측만 막힌 경우
-            pq.remove(right);
-            range.put(right.s, null);
-            Node n1 = new Node(idx, right.e);
-            alloc(n1);
-        } else if (right == null) { //우측만 막힌 경우
-            pq.remove(left);
-            range.put(left.e, null);
-            Node n2 = new Node(left.s, idx);
-            alloc(n2);
-        } else { //양쪽 다 뚫린 경우
-            Node newNode = new Node(left.s, right.e);
-            pq.remove(left);
-            pq.remove(right);
-            range.put(right.s, null);
-            range.put(left.e, null);
-            alloc(newNode);
-        }
-        return count;
-    }
-
-    class Node {
-        int s, e, total;
-
-        public Node(int s, int e) {
-            this.s = s;
-            this.e = e;
-            total = e - s + 1;
+        players = new Player[N + 1];
+        team = new Team[N + 1];
+        for (int i = 1; i <= N; i++) players[i] = new Player(i);
+        for (int i = 1; i <= N; i++) {
+            team[i] = new Team(i); //i번 팀 멤버는 player[i]
+            team[i].add_member(players[i]);
+            players[i].team = team[i];
         }
     }
+
+
+    Team getTeam(int a) {
+        return players[a].team;
+    }
+
+    public void updateScore(int mWinnerID, int mLoserID, int mScore) {
+        Team win = getTeam(mWinnerID);
+        Team lose = getTeam(mLoserID);
+        for (int i = 0; i < win.members.size(); i++) {
+            win.members.get(i).score += mScore;
+        }
+        for (int i = 0; i < lose.members.size(); i++) {
+            lose.members.get(i).score -= mScore;
+        }
+    }
+
+    public void unionTeam(int mPlayerA, int mPlayerB) {
+        int a = getTeam(mPlayerA).team_number;
+        int b = getTeam(mPlayerB).team_number;
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp; //a가 항상 더 작도록
+        }
+        Team aTeam = getTeam(a);
+        Team bTeam = getTeam(b);
+        for (int i = 0; i < bTeam.members.size(); i++) {
+            players[a].team.add_member(bTeam.members.get(i));
+            bTeam.members.get(i).team = aTeam;
+        }
+        team[b] = null;
+    }
+
+    public int getScore(int mID) {
+        return players[mID].score;
+    }
+
+
+    class Player {
+        int index, score;
+        Team team;
+
+        public Player(int index) {
+            this.index = index;
+            score = 0;
+        }
+    }
+
+    class Team {
+        int team_number;
+        List<Player> members;
+
+        public Team(int team_number) {
+            this.team_number = team_number;
+            members = new ArrayList<>();
+        }
+
+        public void add_member(Player p) {
+            members.add(p);
+        }
+
+    }
+
 }
